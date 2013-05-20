180b17aa5978f8e382c3c03b95e108c3bad89414
Josh Hubbard <jhubbardsf@gmail.com>
2013-05-20 15:18:26 -0700

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails', '4.0.0.rc1'
+
+group :development, :test do
+  gem 'rspec-rails', '2.13.1'
+end
+
+group :test do
+  gem 'selenium-webdriver', '2.0'
+  gem 'capybara', '2.1.0.rc1'
+  gem 'factory_girl_rails', '4.2.0'
+end
+
+gem 'mysql2'
+gem 'therubyracer'
+
+gem 'sass-rails', '4.0.0.rc1'
+gem 'haml-rails', '0.4'
+gem 'coffee-rails', '4.0.0'
+gem 'uglifier', '1.0.3'
+
+gem 'jquery-rails', '2.2.1'
+gem 'turbolinks', '1.0.0'
+gem 'jbuilder', '1.0.1'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dmptool2::Application.config.secret_key_base = '9555787b8643f7b2a5cc1896078b81767421f48f39b3f6fecc6362dfbcf6cba5f26341b3e4b689d5788b3ad21be2d903608012243428ea9b4f2cb511a1e21a27'
