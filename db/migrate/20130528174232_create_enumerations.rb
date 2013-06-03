5c1212f93a7ade70804a090c02c53c4216403d39
Josh Hubbard <jhubbardsf@gmail.com>
2013-06-03 09:47:20 -0700

    Fixed spelling error in column.

--- db/migrate/20130528174232_create_enumerations.rb
+++ db/migrate/20130528174232_create_enumerations.rb
@@ -1,7 +1,7 @@
 class CreateEnumerations < ActiveRecord::Migration
   def change
     create_table :enumerations do |t|
-      t.integer :requirements_id
+      t.integer :requirement_id
       t.string :value
 
       t.timestamps
