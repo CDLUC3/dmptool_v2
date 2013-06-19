0f0597005676e7c979b50c0e5844415ff8e746d4
Josh Hubbard <jhubbardsf@gmail.com>
2013-06-19 11:01:38 -0700

    Removed unicorn files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,18 @@
 require 'capistrano-unicorn'
-require "bundler/capistrano"
+#require "bundler/capistrano"
 
 set :application, "dmptool2"
 set :repository,  "git@bitbucket.org:dmptool/dmptool2.git"
 
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
                # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
 set :user, "jhubbard"
 set :use_sudo, true
 set :unicorn_user, 'dmp2'
 #set :unicorn_roles, 'dmp2'
 set :admin_runner, 'dmp2'
 set :runner, 'dmp2'
-set :sudo, 'sudo -u dmp2'
+set :sudo, 'sudo -iu dmp2'
 set :deploy_via, :remote_cache
 set :deploy_to, "/dmp2/apps/dmp2/"
 set :branch, fetch(:branch, "master")
@@ -36,12 +35,5 @@ end
 
 before "deploy:restart", "deploy:symlink_shared"
 
-#after 'deploy:restart', 'unicorn:reload' # app IS NOT preloaded
-after 'deploy:restart', 'unicorn:restart'  # app preloaded
-
-before "deploy:cold",
-       "deploy:install_bundler"
-
-task :install_bundler, :roles => :app do
-  run "type -P bundle &>/dev/null || { gem install bundler --no-rdoc --no-ri; }"
-end
+after 'deploy:restart', 'unicorn:reload' # app IS NOT preloaded
+#after 'deploy:restart', 'unicorn:restart'  # app preloaded
