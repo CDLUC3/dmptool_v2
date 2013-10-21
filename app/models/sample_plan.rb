498e9a098b9e15fa8cebb462fab39eada6e0c08f
Bhavi <bhavi13may@gmail.com>
2013-10-21 13:33:05 -0700

    Validating the presence of a parent model

--- app/models/sample_plan.rb
+++ app/models/sample_plan.rb
@@ -1,3 +1,5 @@
 class SamplePlan < ActiveRecord::Base
-  belongs_to :requirements_template
+  belongs_to :requirements_template, inverse_of: :sample_plans
+
+  validates :requirements_template_id, presence: true, numericality: true
 end
