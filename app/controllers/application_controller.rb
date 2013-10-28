47696405f734416e619c531d169f98b551c39b18
Bhavi <bhavi13may@gmail.com>
2013-10-28 11:24:45 -0700

    Fixing merge conflicts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # enable_authorization
+
   helper_method :current_user
 
   protected
@@ -10,4 +11,5 @@ class ApplicationController < ActionController::Base
     def current_user
       @current_user ||= User.find_by_login_id(session[:login_id]) if session[:login_id]
     end
+
 end
