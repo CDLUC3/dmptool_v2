1e39f64131b596a97445ed4a97132dbc275e7f70
Bhavi <bhavi13may@gmail.com>
2013-10-29 11:47:17 -0700

    Requirements Grouping helper and Resource templates add role related files

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+require "cancan/matchers"
+
+describe Ability do
+
+  ###### TO BE COMPLETED
+
+  # describe "as guest" do
+  #   before(:each) do
+  #     @ability = Ability.new(nil)
+  #   end
+
+  #   it "can only create a user" do
+  #     # Define what a guest can and cannot do
+  #     # @ability.should be_able_to(:create, :users)
+  #     # @ability.should_not be_able_to(:update, :users)
+  #   end
+  # end
+end
