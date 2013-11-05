4d733926e70182752794fc3ecc2718034c60f800
Howard Ding <hding2@illinois.edu>
2013-11-05 11:27:44 -0600

    Code changes for shibboleth via omniauth

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ Bundler.require(:default, Rails.env)
 
 module Dmptool2
   class Application < Rails::Application
+    attr_accessor :shibboleth_host
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
