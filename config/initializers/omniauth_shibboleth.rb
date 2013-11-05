d58355d73cba10e35c697fbf060e4eca632fc4f9
Howard Ding <hding2@illinois.edu>
2013-11-05 13:17:45 -0600

    Fix up for testing - had wrong dns name.

--- config/initializers/omniauth_shibboleth.rb
+++ config/initializers/omniauth_shibboleth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.production?
+  if true #Rails.env.production?
     opts = YAML.load_file(File.join(Rails.root, 'config', 'shibboleth.yml'))[Rails.env]
     provider :shibboleth, opts.symbolize_keys
     Dmptool2::Application.shibboleth_host = opts['host']
