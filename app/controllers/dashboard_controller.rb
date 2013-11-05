f0107b34c29b54a43e54e80a3c20d1741122fca8
Scott Fisher <Scott.Fisher@ucop.edu>
2013-11-05 12:52:42 -0800

    Adding more for dashboard.  Institution plan states.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,12 @@ class DashboardController < ApplicationController
   
   #show the default dashboard with get request
   def show
-    
+    @simple_roles = current_user.roles.map {|i| i.name}
+    if @simple_roles.include?('institutional_reviewer')
+      @pending_review = 0
+      PlanState::PENDING_REVIEW_STATES.each{|s| @pending_review += current_user.institution.plans_by_state(s).count}
+      @finished_review = 0
+      PlanState::FINISHED_REVIEW_STATES.each{|s| @finished_review += current_user.institution.plans_by_state(s).count}
+    end
   end
 end
--- app/models/plan_state.rb
+++ app/models/plan_state.rb
@@ -4,4 +4,10 @@ class PlanState < ActiveRecord::Base
   belongs_to :user
 
   validates_columns :state
+  
+  REVIEW_STATES = ['submitted', 'approved', 'rejected']
+  
+  PENDING_REVIEW_STATES = ['submitted']
+  
+  FINISHED_REVIEW_STATES = ['approved', 'rejected']
 end
