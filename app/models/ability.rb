1207d1095d6f5901694a89f2a36ed19f34b5edf1
shirin238 <cinzia.faenza@gmail.com>
2013-11-06 16:59:13 -0800

    dashboard_header visible only after login

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,7 +12,7 @@ class Ability
   
 
   def initialize(user)
-    @dmp_admin = false
+    
 
     user ||= User.new #guest user if not logged in
 
@@ -20,9 +20,9 @@ class Ability
       can :manage, :all
     end
 
-    if user.has_role?(1)
-      can :edit, :all
-    end
+    # if user.has_role?(1)
+    #   can :edit, :all
+    # end
 
     # if user.has_role?(1)
     #   @editable_user = true
--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,8 @@ Dmptool2::Application.routes.draw do
   get  'remove_resource_editor_role', to: 'resource_templates#remove_resource_editor_role'
   post 'add_role', to: 'resource_templates#add_role'
 
+  #post 'add_role', to: 'roles#add_role'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
