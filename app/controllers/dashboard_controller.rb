dd167699b03f312e28a69a113b1ba11b7f1ecfa0
Scott Fisher <Scott.Fisher@ucop.edu>
2013-11-13 16:53:23 -0800

    Changes to make the dashboard controller show correct stats and link to appropriate places.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,10 +2,16 @@ class DashboardController < ApplicationController
   
   #show the default dashboard with get request
   def show
-    @simple_roles = current_user.roles.map {|i| i.name}
-    if @simple_roles.include?('institutional_reviewer')
+    if safe_has_role?('institutional_reviewer') || safe_has_role?('dmp_administrator')
       @pending_review = current_user.institution.plans_by_state(PlanState::PENDING_REVIEW_STATES).count
       @finished_review = current_user.institution.plans_by_state(PlanState::FINISHED_REVIEW_STATES).count
+    end
+    if safe_has_role?('requirements_editor') || safe_has_role?('dmp_administrator')
+      @requirements_templates = current_user.institution.requirements_templates_deep.where(visibility: 'institutional').count
+      @public_requirements_templates = current_user.institution.requirements_templates_deep.where(visibility: 'public').count
+    end
+    if safe_has_role?('resources_editor') || safe_has_role?('dmp_administrator')
+      @resource_templates = current_user.institution.resource_templates_deep.count
     end
   end
   
