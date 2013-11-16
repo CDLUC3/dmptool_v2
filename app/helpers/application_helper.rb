05615b2464090e69d5b91fefbbb82af016f57759
Daniel Laing <daniel.laing@ucop.edu>
2013-11-15 18:54:06 -0800

    tree view icons, dmp template details page, institution main page first two content tabs, more form styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render(association.to_s.singularize + "_fields", f: builder)
     end
-    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+    link_to(name, '#', class: "add_fields btn", data: {id: id, fields: fields.gsub("\n", "")})
   end
 end
