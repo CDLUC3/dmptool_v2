8d7bd9594d4947c0c7c5feb58ae5c2b99a4ff776
shirin238 <cinzia.faenza@gmail.com>
2013-11-20 09:24:25 -0800

    remove quick-header highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  
   def link_to_add_fields(name, f, association)
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
@@ -7,4 +8,11 @@ module ApplicationHelper
     end
     link_to(name, '#', class: "add_fields btn", data: {id: id, fields: fields.gsub("\n", "")})
   end
+
+  def current_class?(*paths)
+  	current = false
+  	paths.each { |path| current ||= current_page?(path) }
+  	current ? 'current' : nil
+	end
+
 end
