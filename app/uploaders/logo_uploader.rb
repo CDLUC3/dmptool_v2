0aa2b0762d56988957c7c2b4df41f0359ad753e8
shirin238 <cinzia.faenza@gmail.com>
2013-12-02 15:22:43 -0800

    add cache_dir for logo upload

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -9,10 +9,18 @@ class LogoUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+  def cache_dir
+    '/tmp/dmptool2-cache'
+  end
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
+
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+     "/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
