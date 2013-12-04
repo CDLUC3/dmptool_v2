18f9308740dd7316ab5d2851382079a339eff75a
shirin238 <cinzia.faenza@gmail.com>
2013-12-04 15:12:35 -0800

    add generic user tests

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,12 +1,12 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe DashboardController do
+# describe DashboardController do
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
-    end
-  end
+#   describe "GET 'show'" do
+#     it "returns http success" do
+#       get 'show'
+#       response.should be_success
+#     end
+#   end
 
-end
+# end
