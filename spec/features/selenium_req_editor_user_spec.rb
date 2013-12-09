9fade45420af2ead7940e40fb676751b3372d755
shirin238 <cinzia.faenza@gmail.com>
2013-12-09 10:30:08 -0800

    add requirements editor spec

--- spec/features/selenium_req_editor_user_spec.rb
+++ spec/features/selenium_req_editor_user_spec.rb
@@ -5,7 +5,7 @@ include  Credentials
 
 
 
-describe "SeleniumGenericUserSpec" do
+describe "SeleniumReqEditorUserSpec" do
 
   before(:each) do
     @driver = Selenium::WebDriver.for :firefox
@@ -20,37 +20,43 @@ describe "SeleniumGenericUserSpec" do
     @verification_errors.should == []
   end
   
-  it "test_generic_user_spec" do
+  it "test_req_editor_user_spec" do
 
-    #generic user with no specific roles logs in with Test Institution
+    #requirement editor logs in with Smithsonian Institution
     @driver.get(@base_url)
     @driver.find_element(:link, "Log In").click
-    @driver.find_element(:tag_name, "select").find_element(:css,"option[value='#{GENERIC_INSTITUTION_ID}']").click
+    @driver.find_element(:tag_name, "select").find_element(:css,"option[value='#{REQ_EDITOR_INSTITUTION_ID}']").click
     @driver.find_element(:name, "commit").click
-    @driver.find_element(:id, "username").send_keys GENERIC_USERNAME  
-    @driver.find_element(:id, "password").send_keys GENERIC_PASSWORD
+    @driver.find_element(:id, "username").send_keys REQ_EDITOR_USERNAME  
+    @driver.find_element(:id, "password").send_keys REQ_EDITOR_PASSWORD
     @driver.find_element(:name, "commit").click
-    assert_equal "Welcome! #{GENERIC_FIRST_NAME}", @driver.find_element(:css, "p").text
+    assert_equal "Welcome! #{REQ_EDITOR_FIRST_NAME}", @driver.find_element(:css, "p").text
 
-    #he has the generic visibility quick-dashboard
+    #he has requirement editor visibility quick-dashboard
     @driver.find_element(:link, "My Dashboard").text
     @driver.find_element(:link, 'My DMPs').text
     @driver.find_element(:link, 'Create New DMP').text
     @driver.find_element(:link, 'My Profile').text
+    @driver.find_element(:link, 'DMP Templates').text
 
-    @driver.find_element(:css, "a").text.should_not == 'DMP Templates'
     @driver.find_element(:css, "a").text.should_not == 'Resources'
     @driver.find_element(:css, "a").text.should_not == 'Institution Profile' 
     @driver.find_element(:css, "a").text.should_not == 'DMP Administration'  
 
-    #he changes his institution in the My Profile page
+    #he changes his institution in the My Profile page (from SI to Museum of Natural History)
     @driver.find_element(:link, "My Profile").click
-    @driver.find_element(tag_name: 'select').find_element(:css,"option[value='#{GENERIC_INSTITUTION_ID}']").selected?.should == true 
-    #assert_equal "Test Institution", @driver.find_element(:css, "option[value='1']").text   
-    @driver.find_element(:name, "user[institution_id]").find_element(:css,"option[value='#{GENERIC_INSTITUTION_CHILD_ID}']").click
-    @driver.find_element(tag_name: 'select').find_element(:css,"option[value='#{GENERIC_INSTITUTION_CHILD_ID}']").selected?.should == true 
-    @driver.find_element(:name, "user[institution_id]").find_element(:css,"option[value='#{GENERIC_INSTITUTION_ID}']").click
-    @driver.find_element(tag_name: 'select').find_element(:css,"option[value='#{GENERIC_INSTITUTION_ID}']").selected?.should == true 
+    #assert_equal "Smithsonian Institution", @driver.find_element(:css, "option[value='6']").text  
+    @driver.find_element(tag_name: 'select').find_element(:css,"option[value='#{REQ_EDITOR_INSTITUTION_ID}']").selected?.should == true  
+    @driver.find_element(:name, "user[institution_id]").find_element(:css,"option[value='#{REQ_EDITOR_INSTITUTION_CHILD_ID}']").click
+    @driver.find_element(tag_name: 'select').find_element(:css,"option[value='#{REQ_EDITOR_INSTITUTION_CHILD_ID}']").selected?.should == true 
+    @driver.find_element(:name, "user[institution_id]").find_element(:css,"option[value='#{REQ_EDITOR_INSTITUTION_ID}']").click
+    @driver.find_element(tag_name: 'select').find_element(:css,"option[value='#{REQ_EDITOR_INSTITUTION_ID}']").selected?.should == true 
+
+    #creates a new DMP template
+    @driver.find_element(:link, "DMP Templates").click
+    @driver.find_element(:xpath, "//input[@value='Create New template']").click
+    @driver.find_element(:xpath, "(//input[@value='Template Information'])[2]").click
+
 
     
 
