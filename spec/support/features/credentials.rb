05dc506a10e10470d88623a66c792c58dce506d8
shirin238 <cinzia.faenza@gmail.com>
2013-12-09 11:52:16 -0800

    test gitignore

--- spec/support/features/credentials.rb
+++ spec/support/features/credentials.rb
@@ -1,13 +1,6 @@
 
 module Credentials
 
-	GENERIC_USERNAME = 'test_user2'
-	GENERIC_PASSWORD = 'test_user2'
-	GENERIC_FIRST_NAME = 'test_user2'
-	GENERIC_INSTITUTION_NAME = 'Test Institution'
-	GENERIC_INSTITUTION_ID = '1'
-	GENERIC_INSTITUTION_CHILD_NAME = 'Test sub-inst01'
-	GENERIC_INSTITUTION_CHILD_ID = '30'
-
 	
+
 end
