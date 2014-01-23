d6039d4a6dc7a2037db965b4396ca50060474224
Scott Fisher <Scott.Fisher@ucop.edu>
2014-01-22 17:10:16 -0800

    Migration to remove cookie_salt for now.

---
+++ db/migrate/20140123010621_remove_user_cookie_salt.rb
@@ -0,0 +1,9 @@
+class RemoveUserCookieSalt < ActiveRecord::Migration
+  def up
+    remove_column :users, :cookie_salt
+  end
+  def down
+    add_column :users, :cookie_salt, :string
+  end
+  end
+end
