226fba82e03f6913bceb830137716bb5555a1c4e
shirin238 <cinzia.faenza@gmail.com>
2014-01-24 14:12:04 -0800

    fix error when current user and session don't match

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   	def current_user
-    	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+    	@current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   	end
     
     def safe_has_role?(role)
