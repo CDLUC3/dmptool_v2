663581a782f72b8c442603192c7a459ab6e710e4
shirin238 <cinzia.faenza@gmail.com>
2014-01-30 10:12:33 -0800

    fix overview, details and delete links for customizations

---
+++ db/migrate/20140130180737_drop_resource_templates.rb
@@ -0,0 +1,4 @@
+class DropResourceTemplates < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140130010736) do
+ActiveRecord::Schema.define(version: 20140130180737) do
 
   create_table "additional_informations", force: true do |t|
     t.string   "url"
