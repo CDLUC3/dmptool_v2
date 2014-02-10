c8373e84751b51644fe1a5d688505407d86cd84a
Scott Fisher <Scott.Fisher@ucop.edu>
2014-02-10 14:30:06 -0800

    Formatting of template outline and different styling.

--- app/controllers/customizations_controller.rb
+++ app/controllers/customizations_controller.rb
@@ -6,6 +6,9 @@ class CustomizationsController < ApplicationController
     redirect_to(dashboard_path) unless @customization.resource_id.blank? #must be blank, otherwise this isn't a container
     @requirements_template = @customization.requirements_template
     @requirements = @requirements_template.requirements
+    if params[:requirement_id].blank?
+      params[:requirement_id] = @requirements.roots.order(:order).first.id
+    end
   end
 
 end
