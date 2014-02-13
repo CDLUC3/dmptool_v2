03716b6de966ab0867cffccb54ae937fa28ab584
shirin238 <cinzia.faenza@gmail.com>
2014-02-13 09:52:09 -0800

    add fields to select resource page

--- app/controllers/resource_contexts_controller.rb
+++ app/controllers/resource_contexts_controller.rb
@@ -135,6 +135,8 @@ class ResourceContextsController < ApplicationController
     @template_id = params[:template_id]
     @customization_overview_id = params[:customization_overview_id]
 
+    @resource_level = "Template"
+
     @resource_contexts = ResourceContext.includes(:resource).where("resource_id IS NOT NULL")
     @resource_contexts = @resource_contexts.where(institution_id: [current_user.institution.subtree_ids])
 
