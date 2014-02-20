e54bfa38b0ae6ef4de5e390e79d284be04e58355
Scott Fisher <Scott.Fisher@ucop.edu>
2014-02-19 17:25:27 -0800

    Removing turbolinks to fix weird bugginess with redirect_to :back and request.referrer.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,6 @@
 //= require bootstrap
 //= require jquery.ui.all
 //= require jquery_ujs
-//= require turbolinks
-//= require jquery.turbolinks
 //= require ckeditor/override
 //= require ckeditor/init
 //= require_tree .
