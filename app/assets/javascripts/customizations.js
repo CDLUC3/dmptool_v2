f05235b51ce4fd529e23ffbae28485764babdfd7
Scott Fisher <Scott.Fisher@ucop.edu>
2014-02-24 12:08:12 -0800

    Stripping html as appropriate.  Expanding items with more link.  All in customizations.

--- app/assets/javascripts/customizations.js
+++ app/assets/javascripts/customizations.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$( document ).ready(function() {
+  $("a.show_full").click(function(event) {
+    event.preventDefault();
+    t = $(this)
+    t.parent().hide();
+    my_id = t.parent().attr('id').match(/\d+$/)[0];
+    $("#resource_text_full_" + my_id).show("slow", "swing");
+  });
+});
+
