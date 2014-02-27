3e37aac6dc43a340b2989632a76c2ef15480f22e
Scott Fisher <Scott.Fisher@ucop.edu>
2014-02-27 09:04:07 -0800

    Fix for login security on plan#show

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,6 +1,7 @@
 class PlansController < ApplicationController
 
-  before_action :require_login, except: [:public]
+  before_action :require_login, except: [:public, :show]
+  #note show will need to be protected from logins in some cases, but only from non-public plan viewing
   before_action :set_plan, only: [:show, :edit, :update, :destroy, :publish, :export, :details]
   before_action :select_requirements_template, only: [:select_dmp_template]
 
