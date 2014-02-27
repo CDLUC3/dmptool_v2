a4bda73d2834c18a334c9f91b274e57e1480dbc3
shirin238 <cinzia.faenza@gmail.com>
2014-02-26 16:16:25 -0800

    add resource_type filter also when editing a resource

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -34,7 +34,7 @@ class ResourcesController < ApplicationController
   def edit_customization_resource
 
     @resource = Resource.find(params[:id])
-
+    @resource_level = params[:resource_level]
     @customization_id = params[:customization_id]
     @template_id = params[:template_id]
 
@@ -69,6 +69,7 @@ class ResourcesController < ApplicationController
   def update_customization_resource
     @resource = Resource.find(params[:id])
     @customization_id = params[:customization_id]
+    @resource_level = params[:resource_level]
     respond_to do |format|
       if @resource.update(resource_params)
         format.html { redirect_to edit_resource_context_path(@customization_id), notice: 'Resource was successfully updated.' }
