99262a2d260268dcc0e4a5c4b0c4ba335e80cd8f
shirin238 <cinzia.faenza@gmail.com>
2014-02-28 09:26:08 -0800

    add other recipients to notification emails

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -45,7 +45,7 @@ Dmptool2::Application.configure do
     :email => {
       :email_prefix => "[Dmptool2 Exception] ",
       :sender_address => %{"notifier"},
-      :exception_recipients => %w{shirin.faenza@ucop.edu },
+      :exception_recipients => %w{shirin.faenza@ucop.edu Marisa.Strong@ucop.edu Scott.Fisher@ucop.edu Bhavi.Vedula@ucop.edu},
       :sections => %w{request}
     }
 
