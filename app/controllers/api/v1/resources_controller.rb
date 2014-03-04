55e079111f45ee847d3369799deb269bdccb0eb3
shirin238 <cinzia.faenza@gmail.com>
2014-03-04 12:36:31 -0800

    api - add resource show

--- app/controllers/api/v1/resources_controller.rb
+++ app/controllers/api/v1/resources_controller.rb
@@ -4,12 +4,24 @@ class Api::V1::ResourcesController < Api::V1::BaseController
   #   respond_with(Resource.all)
   # end
 
+  # def index
+  #   @resources = Resource.all
+  #   respond_to do |format|
+  #     format.json { render :json => @resources }
+  #   end
+  # end
+
+
   def index
     @resources = Resource.all
-    respond_to do |format|
-      format.json { render :json => @resources }
-    end
+    render json: @resources
   end
 
+  def show
+    @resource = Resource.find(params[:id])
+    # respond_with(@resource, :methods => "last_ticket")
+    render json: @resource
+  end
 
 end
+
