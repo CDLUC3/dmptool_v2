0f33bbb4726b5ee0c17bf73a0d8e5e81b783317a
Bhavi Vedula <bhavi13may@gmail.com>
2014-03-10 12:07:32 -0700

    Displaying Comments on Review page.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+	$("a.show_full").click(function(event){
+    event.preventDefault();
+    t = $(this);
+		t.parent().hide("slow", "swing");
+		my_id = t.parent().attr('id').match(/\d+$/)[0];
+		$("#comment_value_full_" + my_id).show("slow", "swing");
+	});
+
+	$("a.show_less").click(function(event) {
+		event.preventDefault();
+		t = $(this);
+		t.parent().hide("show", "swing");
+		my_id = t.parent().attr('id').match(/\d+$/)[0];
+		$("#comment_value_trunc_" + my_id).show("slow", "swing");
+	});
+});
