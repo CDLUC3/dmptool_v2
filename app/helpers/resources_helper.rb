50712b8a23985fd0b64f75fbe22fc628c687ecb1
Bhavi Vedula <bhavi13may@gmail.com>
2014-04-23 14:05:21 -0700

    Displaying Consistent Resource Customization tabs with "Type" in table

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,2 +1,15 @@
 module ResourcesHelper
+	def resource_type(resource_context)
+		if resource_context.resource.nil?
+			return " "
+		elsif resource_context.resource.resource_type == :help_text
+			return "Guidance"
+		elsif resource_context.resource.resource_type == :actionable_url
+			return "Links"
+		elsif resource_context.resource.resource_type == :suggested_response
+			return "Suggested Response"
+		elsif resource_context.resource.resource_type == :example_response
+			return "Example Response"
+		end
+	end
 end
