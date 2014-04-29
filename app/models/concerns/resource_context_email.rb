edb16d00c3d92cc33b298919ba0f160e4e42e78e
Scott Fisher <Scott.Fisher@ucop.edu>
2014-04-28 18:10:14 -0700

    boilerplate

--- app/models/concerns/resource_context_email.rb
+++ app/models/concerns/resource_context_email.rb
@@ -9,6 +9,14 @@ module ResourceContextEmail
   # for these notifications:
   # [:resource_editors][:deleted] - A customization is deleted
   def resource_context_destroy
-
+    #users = institution.users_in_and_above_inst_in_role(Role::TEMPLATE_EDITOR)
+    #users.delete_if {|u| !u[:prefs][:requirement_editors][:deleted] }
+    #users.each do |user|
+    #  UsersMailer.notification(
+    #      user.email,
+    #      "An institutional DMP template is deleted",
+    #      "requirement_editors_deleted",
+    #      {} ).deliver
+    #end
   end
 end
