5dae7ef12c5991878796b18e5b0431ab7e66855d
Bhavi Vedula <bhavi13may@gmail.com>
2014-05-02 14:34:51 -0700

    Fixing the issue Unit of measure not saving

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -8,7 +8,7 @@ class Requirement < ActiveRecord::Base
   belongs_to :requirements_template
 
   accepts_nested_attributes_for :enumerations, allow_destroy: true, reject_if: proc { |attributes| attributes['value'].blank? }
-  accepts_nested_attributes_for :labels, allow_destroy: true, reject_if: proc { |attributes| attributes['value'].blank? }
+  accepts_nested_attributes_for :labels, allow_destroy: true, reject_if: proc { |attributes| attributes['desc'].blank? }
   default_scope { order('position ASC') }  # requirements should be ordered by position by default, rather than by the id or date
 
   validates_columns :requirement_type, :obligation
