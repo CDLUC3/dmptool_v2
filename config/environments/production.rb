13d2f022fae7429449da8caab1dfcb24ab318807
Marisa Strong <marisa.strong@ucop.edu>
2014-05-08 11:19:02 -0700

    adding js and css to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Dmptool2::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
