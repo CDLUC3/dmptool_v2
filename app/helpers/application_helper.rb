c80edc882bc2f8e92271d6da26fd4a281e5d2431
sfaenza <cinzia.faenza@gmail.com>
2014-05-08 13:37:04 -0700

    my dmps - add toggle sortable name column

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+
+  #to enable sort with toggle effects
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = column == params[:order_scope] && params[:direction] == "asc" ? "desc" : "asc"
+    link_to title, :order_scope => column, :direction => direction, :scope => @scope, :all_scope => @all_scope
+  end
+
   def link_to_add_fields(name, f, association)
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
