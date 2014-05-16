0e5d50cf64cfa0c8e756badbe0679f3b4c3a6c4a
sfaenza <cinzia.faenza@gmail.com>
2014-05-16 10:02:23 -0700

    refactor sql scripts

--- db/migration_script_2.sql
+++ db/migration_script_2.sql
@@ -5,7 +5,7 @@ SET SQL_MODE         = NO_AUTO_VALUE_ON_ZERO;
 SET SQL_SAFE_UPDATES = 0;
 
 #set cutoff date and time for migration
-SET @cutoff = '2014-05-15 17:05:00'; #UTC format
+SET @cutoff = '2014-05-15 17:05:00'; #UTC format, corresponds to timestamp of first migration
 
 
 # ALTER TABLES
