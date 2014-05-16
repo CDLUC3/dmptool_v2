5e70972733f5c3dfd487832e10b323971822345c
sfaenza <cinzia.faenza@gmail.com>
2014-05-16 10:40:30 -0700

    update sql script for deleting duplicate users on dmp2

--- db/remove_duplicate_users.sql
+++ db/remove_duplicate_users.sql
@@ -1,6 +1,7 @@
 SET @cutoff = '2014-05-15 17:05:00'; #UTC format, corresponds to timestamp of first migration
 
-SELECT * 
+DELETE `users_2`
 FROM `dmp2`.`users` AS `users_2`
-JOIN  `dmp`.`authentications` AS `auth_1` ON `users_2`.`login_id` = `auth_1`.`uid`
-WHERE `auth_1`.`created_at` >= @cutoff;
+INNER JOIN  `dmp`.`authentications` AS `auth_1` 
+	ON `users_2`.`login_id` = `auth_1`.`uid`
+WHERE `auth_1`.`created_at` > @cutoff;
