2400f63cf9c3715b1f9ebe3b29dc2e26506fff5a
Scott Fisher <Scott.Fisher@ucop.edu>
2014-05-20 16:52:09 -0700

    Adding who, what when data for users into EZID as mentioned by Perry.

--- lib/ezid.rb
+++ lib/ezid.rb
@@ -10,20 +10,20 @@ module Ezid
       @uri, @user, @pwd = URI.parse(uri_string), username, pwd
     end
 
-    def mint
-      return mint_it
+    def mint_erc(who, what, whn)
+      return mint_it("erc.who: #{escape(who)}\nerc.what: #{escape(what)}\nerc.when: #{escape(whn)}")
     end
 
     private
 
-    def mint_it
+    def mint_it(body_metadata)
       begin
         Net::HTTP.start(@uri.host, @uri.port,
             :use_ssl => @uri.scheme == 'https',
             :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
 
           request = Net::HTTP::Post.new(@uri.request_uri, initheader = { 'Content-Type' => 'text/plain; charset=UTF-8'})
-          #request.body = "_target: http://www.cdlib.org/\r\nerc.who: Bob"
+          request.body = body_metadata
           request.basic_auth @user, @pwd
 
           response = http.request request
@@ -31,14 +31,19 @@ module Ezid
           if response.body.start_with?('success:')
             return response.body.split()[1]
           else
-            raise MintException.new("Minting unsuccessful.")
+            raise MintException.new("Minting unsuccessful: #{response.body}")
           end
         end
       rescue SocketError
         raise MintException.new("Could not connect to server.")
-      rescue Exception
+      rescue Exception => ex
         raise MintException.new("Can't get ID; not a EZID server?")
       end
+    end
+
+    def escape(str)
+      str.gsub('%', '%25').gsub('\n', '%0A').
+          gsub('\r', '%0D').gsub(':', '%3A')
     end
   end
 end
