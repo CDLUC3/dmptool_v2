5079899ed0378ccb918e0c237bac4eb8e9cb32b3
sfaenza <cinzia.faenza@gmail.com>
2014-05-20 15:35:02 -0700

    add commented master branch configurations

--- config/application.rb
+++ config/application.rb
@@ -41,3 +41,52 @@ module Dmptool2
     
   end
 end
+
+
+
+#master application.rb
+
+# require File.expand_path('../boot', __FILE__)
+
+# # Pick the frameworks you want:
+# require "active_record/railtie"
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "sprockets/railtie"
+# # require "rails/test_unit/railtie"
+# require 'yaml'
+
+# # Require the gems listed in Gemfile, including any gems
+# # you've limited to :test, :development, or :production.
+# Bundler.require(:default, Rails.env)
+
+# module Dmptool2
+#   class Application < Rails::Application
+#     attr_accessor :shibboleth_host
+#     # Settings in config/environments/* take precedence over those specified here.
+#     # Application configuration should go into files in config/initializers
+#     # -- all .rb files in that directory are automatically loaded.
+
+#     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+#     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+#     # config.time_zone = 'Central Time (US & Canada)'
+
+#     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+#     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+#     # config.i18n.default_locale = :de
+#     config.i18n.enforce_available_locales = true
+# #   config.assets.precompile += %w(*.js *.css)
+# #   config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+# #    config.assets.precompile += Ckeditor.assets
+# #   config.assets.precompile += %w('application.js', 'application.css')
+#     config.assets.precompile += ['ckeditor/*']
+#     config.autoload_paths += %W(#{config.root}/lib)
+#     config.assets.compile = true
+#     config.assets.debug = false
+
+#     config.exceptions_app = self.routes
+
+    
+#   end
+# end
+
--- config/deploy.rb
+++ config/deploy.rb
@@ -28,3 +28,37 @@ namespace :deploy do
     run "ln -s #{shared_path}/uploads #{release_path}/public/uploads"
   end
 end
+
+# master deploy.rb
+
+# require 'capistrano/ext/multistage'
+# require 'capistrano-unicorn'
+# require 'bundler/capistrano'
+
+# set :application, 'dmptool2'
+# set :repository,  'git@bitbucket.org:dmptool/dmptool2.git'
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+
+# set :user, 'dmp2'
+# set :deploy_via, :remote_cache
+# set :branch, fetch(:branch, 'master')
+# set :env, fetch(:env, 'development')
+# set :rails_env, fetch(:renv, 'development')
+# set :ssh_options, { :forward_agent => true }
+# set :use_sudo, false
+
+# default_run_options[:env] = { 'PATH' => '/dmp2/local/bin/:$PATH'}
+# default_run_options[:pty] = true
+
+
+# namespace :deploy do
+#   task :symlink_shared do
+#     run "ln -s #{shared_path}/database.yml #{release_path}/config/"
+#     run "ln -s #{shared_path}/shibboleth.yml #{release_path}/config/"
+#     run "ln -s #{shared_path}/ldap.yml #{release_path}/config/"
+#     run "ln -s #{shared_path}/unicorn.rb #{release_path}/config/"
+#     run "ln -s #{shared_path}/uploads #{release_path}/public/uploads"
+#   end
+# end
+
