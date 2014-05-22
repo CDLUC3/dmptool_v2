ea90179037b484676fae3f892e2a1f1f7564541c
sfaenza <cinzia.faenza@gmail.com>
2014-05-22 10:31:25 -0700

    add logo validation

--- app/assets/javascripts/institutions.js
+++ app/assets/javascripts/institutions.js
@@ -67,19 +67,31 @@ $(function() {
 	});
 });
 
-// $(function() {
-// 	$('#current_admin_with_alert').click(function(){
-// 		if (!$('#5').is(':checked')) { 
-// 			bootbox.confirm( "Do you really want to remove yourself from the Institutional Administrator role? You will lose all administrative permissions.", function(result){
-// 				if (result) {
-// 					return true;
-// 				} else {
-// 					return false;
-// 				}
-// 			});
-// 		};
-// 	});
-// });
+function validateFiles(inputFile) {
+  // var maxExceededMessage = "This file exceeds the maximum allowed file size (5 MB)";
+  var extErrorMessage = "Only image file with extension: .jpg, .jpeg, .gif or .png is allowed";
+  var allowedExtension = ["jpg", "jpeg", "gif", "png"];
+ 
+  var extName;
+  // var maxFileSize = $(inputFile).data('max-file-size');
+  // var sizeExceeded = false;
+  var extError = false;
+ 
+  $.each(inputFile.files, function() {
+    // if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};
+    extName = this.name.split('.').pop();
+    if ($.inArray(extName, allowedExtension) == -1) {extError=true;};
+  });
+  // if (sizeExceeded) {
+  //   window.alert(maxExceededMessage);
+  //   $(inputFile).val('');
+  // };
+ 
+  if (extError) {
+    window.alert(extErrorMessage);
+    $(inputFile).val('');
+  };
+}
 
 
 
