aa4e1b1ffdf39c7d0cafdbf07cac42d1f883dfa5
Bhavi Vedula <bhavi13may@gmail.com>
2014-05-22 12:15:01 -0700

    Revised state of a plan on editing responses.

--- app/models/response.rb
+++ app/models/response.rb
@@ -6,7 +6,7 @@ class Response < ActiveRecord::Base
   validates :plan_id, presence: true, numericality: true
   validates :requirement_id, presence: true, numericality: true
   validates :text_value, presence: true, if: :validate_only_if_obligation_mandatory
-
+  #after_update :associated_responses
 
 
   def validate_only_if_obligation_mandatory
@@ -25,6 +25,19 @@ class Response < ActiveRecord::Base
   rescue ActiveRecord::StaleObjectError
       flash[:error] = "This record changed while you were editing."
       false
+  end
+
+  def associated_responses
+    plan = Plan.find(self.plan_id)
+    plan_state_id = plan.current_plan_state_id
+    state = PlanState.find(plan_state_id).state
+    if (state  == :committed) || (state == :approved) || (state == :rejected) || (state == :reviewed)
+      if self.previous_changes != ""
+        return self.id
+      else
+        return ""
+      end
+    end
   end
 
 end
