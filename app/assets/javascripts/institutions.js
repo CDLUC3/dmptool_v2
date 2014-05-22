2720bc260f59b7b0c8ac3b3704a5fad335083915
sfaenza <cinzia.faenza@gmail.com>
2014-05-22 11:27:15 -0700

    change alert box for logo validation

--- app/assets/javascripts/institutions.js
+++ app/assets/javascripts/institutions.js
@@ -68,27 +68,19 @@ $(function() {
 });
 
 function validateFiles(inputFile) {
-  // var maxExceededMessage = "This file exceeds the maximum allowed file size (5 MB)";
   var extErrorMessage = "Only image file with extension: .jpg, .jpeg, .gif or .png is allowed";
   var allowedExtension = ["jpg", "jpeg", "gif", "png"];
- 
   var extName;
-  // var maxFileSize = $(inputFile).data('max-file-size');
-  // var sizeExceeded = false;
   var extError = false;
  
   $.each(inputFile.files, function() {
-    // if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};
     extName = this.name.split('.').pop();
     if ($.inArray(extName, allowedExtension) == -1) {extError=true;};
   });
-  // if (sizeExceeded) {
-  //   window.alert(maxExceededMessage);
-  //   $(inputFile).val('');
-  // };
  
   if (extError) {
-    window.alert(extErrorMessage);
+    // window.alert(extErrorMessage);
+    $.alert("extErrorMessage", "Wrong File Extension")
     $(inputFile).val('');
   };
 }
