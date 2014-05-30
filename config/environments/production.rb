5d64dac1e7054d183262c9a2fb751dde0121b166
Scott Fisher <Scott.Fisher@ucop.edu>
2014-05-30 16:02:05 -0700

    This is a change to put the config for the email host name in the app_config.yml file so it's obvious it should be changed if the domain name also changes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,5 @@ Dmptool2::Application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => "https://dmp2.cdlib.org/" }
 
 end
--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,4 +1,5 @@
 # config/initializers/load_config.rb
 if File.exists?(File.join(Rails.root, 'config', 'app_config.yml'))
   APP_CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'app_config.yml'))[Rails.env]
+  ActionMailer::Base.default_url_options = { :host => APP_CONFIG['host_for_email_links'] }
 end
