6f1ba81c6808c33eecee6d97a6438daf39b46ab1
sfaenza <cinzia.faenza@gmail.com>
2014-08-18 11:46:19 -0700

    fix users api

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -13,4 +13,56 @@
 
 class Api::V1::BaseController < ActionController::Base
 	respond_to :json
+
+
+
+
+
+	def require_admin
+    unless user_role_in?(:dmp_admin)
+      flash[:error] = "You must be an administrator to access this page."
+      session[:return_to] = request.original_url
+      redirect_to choose_institution_path and return
+    end
+  end
+
+  def safe_has_role?(role)
+    #this returns whether a user has a role, but does it safely.  If no user is logged in
+    #then it returns false by default.  Will work with either number or more readable role name.
+    return false if current_user.nil?
+    if role.class == Fixnum || (role.class == String && role.match(/^[-+]?[1-9]([0-9]*)?$/) )
+      current_user.has_role?(role)
+    else
+      current_user.has_role_name?(role)
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  def user_role_in?(*roles)
+    if (roles - ROLES.keys).length > 0
+      raise "role not defined in application_controller#user_role_in?.  It's likely you've mistyped a role symbol."
+    end
+    return false if current_user.nil?
+    r = roles.map {|i| ROLES[i]}
+    matching_roles = current_user.roles.pluck(:id) & r
+    return true if matching_roles.length > 0
+    return false
+  end
+
+  ROLES =
+      {   :dmp_admin              => Role::DMP_ADMIN,
+          :resource_editor        => Role::RESOURCE_EDITOR,
+          :template_editor        => Role::TEMPLATE_EDITOR,
+          :institutional_reviewer => Role::INSTITUTIONAL_REVIEWER,
+          :institutional_admin    => Role::INSTITUTIONAL_ADMIN}
+
+
+
+
+
+	
 end
+
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::UsersController < Api::V1::BaseController
 	before_action :require_admin
 
+
+
 	respond_to :json
 
   	def index         
@@ -8,31 +10,11 @@ class Api::V1::UsersController < Api::V1::BaseController
   	end 
 
 	def show
-    	@user = User.find(params[:id])
-  	end
-
-
-  def require_admin
-    unless user_role_in?(:dmp_admin)
-      flash[:error] = "You must be an administrator to access this page."
-      session[:return_to] = request.original_url
-      redirect_to choose_institution_path and return
-    end
+    @user = User.find(params[:id])
   end
 
-  def safe_has_role?(role)
-    #this returns whether a user has a role, but does it safely.  If no user is logged in
-    #then it returns false by default.  Will work with either number or more readable role name.
-    return false if current_user.nil?
-    if role.class == Fixnum || (role.class == String && role.match(/^[-+]?[1-9]([0-9]*)?$/) )
-      current_user.has_role?(role)
-    else
-      current_user.has_role_name?(role)
-    end
-  end
 
-  def current_user
-    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-  end
+
+
 
 end
