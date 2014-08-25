ba652aadd9d7860a4d34a6bebe15c4fa94777819
sfaenza <cinzia.faenza@gmail.com>
2014-08-25 12:24:28 -0700

    add api test for institutions json

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -1,14 +1,31 @@
 class Api::V1::PlansController < Api::V1::BaseController
 
-	respond_to :json
+	# respond_to :json
 
-  	def index         
-    	@plans = Plan.all     
-  	end 
+ #  	def index         
+ #    	@plans = Plan.all     
+ #  	end 
+
+ #respond_to :json
+
+   	def index         
+     	@plans = Plan.all 
+
+     	respond_to do |format|
+     		format.json {render json: @plans, status: 200}
+     		format.xml {render xml: @plans, status: 200}
+     	end
+
+   	end 
 
 	def show
-    	@plan = Plan.find(params[:id])
-  	end
+    @plan = Plan.find(params[:id])
+
+    respond_to do |format|
+     	format.json {render json: @plan, status: 200}
+     	format.xml {render xml: @plan, status: 200}
+    end
+  end
 
 
 end
