e71acc010dd75c9a4abaca7b4f9f16e1c96d0197
Scott Fisher <Scott.Fisher@ucop.edu>
2014-09-03 10:34:48 -0700

    Authenticate with LDAP.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::UsersController < Api::V1::BaseController
-	# before_action :authenticate
+	before_action :authenticate
 
-	http_basic_authenticate_with name: 'admin', password: 'secret'
+	#http_basic_authenticate_with name: 'admin', password: 'secret'
 
 	respond_to :json
 
 	def index         
-  	@users = User.all     
+  	@users = User.all
 	end 
 
 	def show
@@ -14,12 +14,12 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
 
-  # protected
-  # def authenticate
-  # 	authenticate_or_request_with_http_basic do |username, password|
-  # 		Ldap_User.valid_ldap_credentials?(username, password)
-  # 	end
-  # end
+  protected
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      Ldap_User.valid_ldap_credentials?(username, password)
+    end
+  end
 
 
 
