779210fbecc071c0808e81bb314fb5cffa541d44
sfaenza <cinzia.faenza@gmail.com>
2014-09-15 15:37:35 -0700

    update plans api

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -1,30 +1,27 @@
 class Api::V1::PlansController < Api::V1::BaseController
 
-	# respond_to :json
 
- #  	def index         
- #    	@plans = Plan.all     
- #  	end 
+ 	def index         
+   	@plans = Plan.all 
 
- #respond_to :json
+   	# respond_to do |format|
+   	# 	format.json {render json: @plans, status: 200}
+   	# 	format.xml {render xml: @plans, status: 200}
+   	# end
 
-   	def index         
-     	@plans = Plan.all 
+    render json: @plans, status: 200
 
-     	respond_to do |format|
-     		format.json {render json: @plans, status: 200}
-     		format.xml {render xml: @plans, status: 200}
-     	end
-
-   	end 
+ 	end 
 
 	def show
     @plan = Plan.find(params[:id])
 
-    respond_to do |format|
-     	format.json {render json: @plan, status: 200}
-     	format.xml {render xml: @plan, status: 200}
-    end
+    # respond_to do |format|
+    #  	format.json {render json: @plan, status: 200}
+    #  	format.xml {render xml: @plan, status: 200}
+    # end
+
+    render json: @plan, status:200
   end
 
 
