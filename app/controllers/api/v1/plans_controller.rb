f5cdb6ad82085cd95ad81451a064a286d94d9c42
sfaenza <cinzia.faenza@gmail.com>
2014-09-30 10:29:43 -0700

    plans - api - add institutional and private plans to index action

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -2,7 +2,10 @@ class Api::V1::PlansController < Api::V1::BaseController
 	before_action :authenticate
 
  	def index         
-   	@plans = Plan.all.public_visibility 
+   	@public_plans = Plan.all.public_visibility
+    @private_plans = Plan.all.private_visibility.joins(:users).where(user_plans: {owner: true}).where('users.id =?', current_user.id) 
+    @institutional_plans = Plan.all.institutional_visibility.joins(:requirements_template).where('requirements_templates.institution_id =?', current_user.institution_id)
+    @plans = @public_plans + @private_plans + @institutional_plans
     if from_date = params[:from_date]
       @plans = @plans.where(["created_at > ?", from_date])
     end
