729403a284ec9cb30a47471b7971b5b82d0b27cd
sfaenza <cinzia.faenza@gmail.com>
2014-10-01 11:17:39 -0700

    API - use find_by_id instead of find in show methods to avoid ActiveRecord exceptions

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -4,27 +4,30 @@ class Api::V1::PlansController < Api::V1::BaseController
 
 
  	def index         
-   	@public_plans = Plan.all.public_visibility
-    @private_plans = Plan.all.private_visibility.joins(:users).where(user_plans: {owner: true}).where('users.id =?', current_user.id) 
-    @institutional_plans = Plan.all.institutional_visibility.joins(:requirements_template).where('requirements_templates.institution_id =?', current_user.institution_id)
-    @plans = @public_plans + @private_plans + @institutional_plans
-    if from_date = params[:from_date]
-      @plans = @plans.where(["created_at > ?", from_date])
-    end
-    render json: @plans, status: 200
+       	@public_plans = Plan.all.public_visibility
+        @private_plans = Plan.all.private_visibility.joins(:users).where(user_plans: {owner: true}).where('users.id =?', current_user.id) 
+        @institutional_plans = Plan.all.institutional_visibility.joins(:requirements_template).where('requirements_templates.institution_id =?', current_user.institution_id)
+        @plans = @public_plans + @private_plans + @institutional_plans
+        if from_date = params[:from_date]
+          @plans = @plans.where(["created_at > ?", from_date])
+        end
+        render json: @plans, status: 200
  	end 
 
 
 	def show
-    @plan = Plan.find(params[:id])
-    if (@plan.visibility == :public)  ||  
-    		((@plan.visibility == :institutional) && (current_user.institution_id == @plan.requirements_template.institution_id)) ||
-        ( (@plan.visibility == :private) && ( @plan.users.include?(current_user)) )
-    	render json: @plan, status:200
-    else
-    	 render json: 'You are not authorized to look at this content', status: 401
-    end 
-  end
+        if @plan = Plan.find_by_id(params[:id])
+            if (@plan.visibility == :public)  ||  
+            		((@plan.visibility == :institutional) && (current_user.institution_id == @plan.requirements_template.institution_id)) ||
+                ( (@plan.visibility == :private) && ( @plan.users.include?(current_user)) )
+            	render json: @plan, status:200
+            else
+            	 render json: 'You are not authorized to look at this content', status: 401
+            end
+        else
+            render json: 'The plan you are looking for doesn\'t exist', status: 404
+        end
+    end
 
 
 end
