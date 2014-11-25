a2d4938fc987310a040d7da1311c032483ae668b
sfaenza <cinzia.faenza@gmail.com>
2014-11-25 10:15:05 -0800

    api - add 404 message in institutions_admins_count when institution doesn't exist

--- app/controllers/api/v1/institutions_controller.rb
+++ app/controllers/api/v1/institutions_controller.rb
@@ -28,7 +28,11 @@ class Api::V1::InstitutionsController < Api::V1::BaseController
 
 
 	def admins_count_show
-  	@institution = Institution.find_by_id(params[:id])
+  	if @institution = Institution.find_by_id(params[:id]) 
+      @institution 
+    else
+      render json: 'The institution you are looking for doesn\'t exist', status: 404
+    end
 	end 
 
 
