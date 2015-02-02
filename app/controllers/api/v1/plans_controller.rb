de32dddc1ea6548818176ce49808cfa7163545b4
sfaenza <cinzia.faenza@gmail.com>
2015-02-02 15:28:22 -0800

    plans api - add institutional hierarchy

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -11,9 +11,13 @@ class Api::V1::PlansController < Api::V1::BaseController
  	def index
         if @user = User.find_by_id(session[:user_id])
            	@public_plans = Plan.all.public_visibility
-            @private_plans = Plan.all.private_visibility.joins(:users).where(user_plans: {owner: true}).where('users.id =?', current_user.id) 
-            @institutional_plans = Plan.all.institutional_visibility.joins(:requirements_template).where('requirements_templates.institution_id =?', current_user.institution_id)
-            @plans = @public_plans + @private_plans + @institutional_plans
+            
+            @owned_private_plans = @user.owned_plans.private_visibility
+            @coowned_private_plans = @user.coowned_plans.private_visibility
+
+            @institutional_plans = Plan.institutional_visibility.joins( {:users => :institution} ).where("user_plans.owner = 1").where("users.institution_id IN (?)", @user.institution.path_ids)
+
+            @plans = @public_plans + @owned_private_plans + @coowned_private_plans + @institutional_plans
             if from_date = params[:from_date]
               @plans = @plans.where(["created_at > ?", from_date])
             end
@@ -25,20 +29,20 @@ class Api::V1::PlansController < Api::V1::BaseController
 
 
 	def show
-
         if @user = User.find_by_id(session[:user_id])
             if @plan = Plan.find_by_id(params[:id])
                 @id = @plan.id
+
                 if (@plan.visibility == :public)  ||  
-                		((@plan.visibility == :institutional) && (current_user.institution_id == @plan.requirements_template.institution_id)) ||
-                    ( (@plan.visibility == :private) && ( @plan.users.include?(current_user)) )
+                    ( (@plan.visibility == :institutional) && ( @user.institution.path_ids.include?(@plan.owner.institution_id) || @plan.coowners.include?(@user)  ) ) ||
+                    ( (@plan.visibility == :private) && ( @user == @plan.owner || @plan.coowners.include?(@user)) )
                 	
                     @plan
-
 
                 else
                 	 render json: 'You are not authorized to look at this content.', status: 401
                 end
+
             else
                 render json: 'The plan you are looking for doesn\'t exist', status: 404
             end
@@ -54,11 +58,7 @@ class Api::V1::PlansController < Api::V1::BaseController
             else
                 render json: 'The plan you are looking for doesn\'t exist', status: 404
             end
-
         end
-
-
-
     end
 
 
