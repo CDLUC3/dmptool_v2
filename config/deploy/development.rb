4dbe0d2df83478cca5f7eaa37d8714bd525bfabd
briley <brian.riley@ucop.edu>
2016-02-22 13:59:45 -0800

    Updated config/deploy/development.rb to point to the new AWS server

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,9 +1,9 @@
-role :web, 'dmp2-dev.cdlib.org'
-role :app, 'dmp2-dev.cdlib.org'
-role :db,  'dmp2-dev.cdlib.org', :primary => true # This is where Rails migrations will run
+role :web, 'uc3-dmp2-dev.cdlib.org'
+role :app, 'uc3-dmp2-dev.cdlib.org'
+role :db,  'uc3db-dmp2-dev.cdlib.org', :primary => true # This is where Rails migrations will run
 
-set :deploy_to, "/dmp2/apps/dmp2/"
-set :unicorn_pid, "/dmp2/apps/dmp2/shared/tmp/unicorn.dmp2.pid"
+set :deploy_to, "/apps/dmp2/apps/dmp2/"
+set :unicorn_pid, "/apps/dmp2/apps/dmp2/shared/tmp/unicorn.dmp2.pid"
 
 after 'deploy', 'deploy:migrate'
 after 'deploy:finalize_update', 'deploy:symlink_shared'
