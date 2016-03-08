74b48e1adbda011bd28a7cbf1aac0563454ade75
briley <brian.riley@ucop.edu>
2016-03-08 11:01:20 -0800

    Updated stage and prod environment files

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,10 +2,11 @@ role :web, 'uc3-dmp2-stg.cdlib.org'
 role :app, 'uc3-dmp2-stg.cdlib.org'
 role :db,  'uc3-dmp2-stg.cdlib.org', :primary => true # This is where Rails migrations will run
 
-set :deploy_to, "/apps/dmp2/apps/dmp2/"
-set :unicorn_pid, "/apps/dmp2/apps/dmp2/shared/tmp/unicorn.dmp2.pid"
+set :deploy_to, "/dmp2/apps/dmp2/"
+set :unicorn_pid, "/dmp2/apps/dmp2/shared/tmp/unicorn.dmp2.pid"
 set :rails_env, "stage"
 
+before 'bundle:install', 'deploy:prepare_bundle_config'
 
 after 'deploy', 'deploy:migrate'
 after 'deploy:finalize_update', 'deploy:symlink_shared'
