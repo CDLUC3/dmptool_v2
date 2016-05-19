e3bc99776f97d9f209b209437c4e3b41f267af81
briley <brian.riley@ucop.edu>
2016-05-19 15:08:40 -0700

    Added an email interceptor to reroute outgoing emails in test/dev environment to be sent to developers

---
+++ config/initializers/email_interceptor.rb
@@ -0,0 +1,11 @@
+if Rails.env.development? or Rails.env.test?
+  ActionMailer::Base.register_interceptor(EmailInterceptor)
+end
+
+class EmailInterceptor
+  # Intercepts outgoing emails and changes the recipient to the developers
+  def self.delivering_email(message)
+    message.to = ['brian.riley@ucop.edu']
+  end
+end
+
