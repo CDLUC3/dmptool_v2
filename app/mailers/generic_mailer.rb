83bd13edb68c28425394eb0a2758c505c40f4748
Brian Riley <riley.bri@gmail.com>
2016-05-20 15:19:59 -0700

    Updted mailer to use the 'from' address as the 'reply-to' address and updated the config to change the from address to do-not-reply-dmptool@ucop.edu

--- app/mailers/generic_mailer.rb
+++ app/mailers/generic_mailer.rb
@@ -5,9 +5,9 @@ class GenericMailer < ActionMailer::Base
     # values :question_about, :name, :email, :message
     @form_hash = form_hash
     mail :to => email,
-         :reply_to => form_hash[:email],
          :subject => 'DMPTool2 Contact Us Form Feedback',
-         :from => APP_CONFIG['feedback_email_from']
+         :from => APP_CONFIG['feedback_email_from'],
+         :reply_to => APP_CONFIG['feedback_email_from'] #form_hash[:email],
   end
 
 end
