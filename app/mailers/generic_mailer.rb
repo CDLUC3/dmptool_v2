34f31b8dfdfa1431092df585cbb622e2a8f44788
briley <brian.riley@ucop.edu>
2016-05-23 09:23:59 -0700

    switched reply-to address in the generic mailer to match the 'from' address. Also added some logging

--- app/mailers/generic_mailer.rb
+++ app/mailers/generic_mailer.rb
@@ -2,12 +2,15 @@ class GenericMailer < ActionMailer::Base
   default :from => APP_CONFIG['feedback_email_from']
 
   def contact_email(form_hash, email)
+    
+logger.warn "Sending Email to: #{email}, from: #{APP_CONFIG['feedback_email_from']}"
+    
     # values :question_about, :name, :email, :message
     @form_hash = form_hash
     mail :to => email,
          :subject => 'DMPTool2 Contact Us Form Feedback',
          :from => APP_CONFIG['feedback_email_from'],
-         :reply_to => APP_CONFIG['feedback_email_from'] #form_hash[:email],
+         :reply_to => APP_CONFIG['feedback_email_from'] #form_hash[:email]
   end
 
 end
