c7ca7ec338e2df5ae7b271faf03f061e4036a5fe
briley <brian.riley@ucop.edu>
2016-06-30 11:15:27 -0700

    query not working so modified where and joins clauses for previously reviewed plans

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -36,7 +36,7 @@ class Plan < ActiveRecord::Base
   scope :revised, -> { joins(:current_state).where('plan_states.state =?', :revised) }
   scope :committed, -> { joins(:current_state).where('plan_states.state =?', :committed) }
   scope :reviewed, -> { joins(:current_state).where('plan_states.state =?', :reviewed) }
-  scope :previously_reviewed, -> { joins(:current_state).where('plan_states.state =?', :reviewed).joins(:plan_states).where('plan_states.state =?', :reviewed) }
+  scope :previously_reviewed, -> { joins(:plan_states).where(state: 'reviewed').joins(:current_state).where('plan_states.state =?', :committed) }
   
   # scopes for Plan Review
   scope :plans_to_be_reviewed, ->(institution_id) {joins(:users, :current_state).where("users.institution_id IN(?)", institution_id).where(plan_states: {state: 'submitted'}).where(user_plans: {owner: true})}
@@ -44,7 +44,7 @@ class Plan < ActiveRecord::Base
   scope :plans_rejected, ->(institution_id) {joins(:users, :current_state).where("users.institution_id IN(?)", institution_id).where(plan_states: {state: 'rejected'}).where(user_plans: {owner: true})}
   scope :plans_reviewed, ->(institution_id) {joins(:users, :current_state).where("users.institution_id IN(?)", institution_id).where(plan_states: {state: 'reviewed'}).where(user_plans: {owner: true})}
   scope :plans_per_institution, ->(institution_id) {joins(:users, :current_state).where("users.institution_id IN(?)", institution_id).where(plan_states: {state: ['rejected', 'approved', 'submitted', 'reviewed']}).where(user_plans: {owner: true})}
-  scope :plans_previously_reviewed, ->(institution_id) {joins(:users, :current_state).where("users.institution_id IN(?)", institution_id).where(plan_states: {state: 'committed'}).where(user_plans: {owner: true}).joins(:plan_states).where('plan_states.state =?', :reviewed)}
+  scope :plans_previously_reviewed, ->(institution_id) {joins(:users, :plan_states).where("users.institution_id IN(?)", institution_id).where(state: 'reviewed').where(user_plans: {owner: true}).joins(:current_state).where(plan_states: {state: 'committed'})}
   
   def plan_responses_ids
     @response_ids = [] 
