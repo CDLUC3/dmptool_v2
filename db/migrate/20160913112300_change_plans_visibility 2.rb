f423adbf06b1194c35083600a213a62674ceae4c
briley <brian.riley@ucop.edu>
2016-09-13 11:33:04 -0700

    renamed db migration file

--- db/migrate/20160913112300_change_plans_visibility 2.rb
+++
@@ -1,5 +0,0 @@
-class ChangePlansVisibility < ActiveRecord::Migration
-  def change
-    change_column :plans, :visibility, :enum, limit: [:institutional, :public, :private, :unit, :test]
-  end
-end
