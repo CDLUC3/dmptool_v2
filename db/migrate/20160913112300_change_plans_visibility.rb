92bf87ff0b88d79da4e4231e3a8ba9f610067063
briley <brian.riley@ucop.edu>
2016-09-13 13:56:28 -0700

    fixed additional typos in data management guidance static page

---
+++ db/migrate/20160913112300_change_plans_visibility.rb
@@ -0,0 +1,5 @@
+class ChangePlansVisibility < ActiveRecord::Migration
+  def change
+    change_column :plans, :visibility, :enum, limit: [:institutional, :public, :private, :unit, :test]
+  end
+end
