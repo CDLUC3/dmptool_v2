18c0a7de282f1aa02cea22d4c6a8c0f251b6db3b
briley <brian.riley@ucop.edu>
2016-10-31 09:37:56 -0700

    hide requirement delete links

--- app/helpers/requirements_helper.rb
+++ app/helpers/requirements_helper.rb
@@ -4,4 +4,10 @@ module RequirementsHelper
       render(requirement) + content_tag(:div, nested_requirements(sub_requirements), :class => "nested_requirements")
     end.join.html_safe
   end
+  
+  def nested_requirements_view(requirements)
+    requirements.map do |requirement, sub_requirements|
+      render(requirement)
+    end.join.html_safe
+  end
 end
