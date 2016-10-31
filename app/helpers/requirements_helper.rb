63b5fe1abc8e5c021850ae3dfcb34cd83b4e0ef5
briley <brian.riley@ucop.edu>
2016-10-31 10:08:34 -0700

    tweak to the way requirements are listed

--- app/helpers/requirements_helper.rb
+++ app/helpers/requirements_helper.rb
@@ -4,10 +4,4 @@ module RequirementsHelper
       render(requirement) + content_tag(:div, nested_requirements(sub_requirements), :class => "nested_requirements")
     end.join.html_safe
   end
-  
-  def nested_requirements_view(requirements)
-    requirements.map do |requirement, sub_requirements|
-      render(requirement_readonly)
-    end.join.html_safe
-  end
 end
