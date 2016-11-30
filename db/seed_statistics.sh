88b570919ff2a29a58d7d8fddb6b274ddfbc70d6
briley <brian.riley@ucop.edu>
2016-11-30 13:22:34 -0800

    Added 'bundle exec' to statistics seed script'

--- db/seed_statistics.sh
+++ db/seed_statistics.sh
@@ -7,6 +7,6 @@ do
   for MONTH in 1 2 3 4 5 6 7 8 9 10 11 12
   do
     echo "Generating statistics for $YEAR-$MONTH"
-    rake statistics:generate[$YEAR,$MONTH]
+    bundle exec rake statistics:generate[$YEAR,$MONTH]
   done
 done
