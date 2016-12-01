977beb93bc524c21e98e24e8825e3e8584b1e86f
briley <brian.riley@ucop.edu>
2016-12-01 09:18:21 -0800

    fixed issue with usage stat run_date jquery

--- app/assets/javascripts/institutions.js
+++ app/assets/javascripts/institutions.js
@@ -98,7 +98,7 @@ function validateFiles(inputFile) {
 
 $(function(){
 	$("#run_date").on('change', function(e){
-		var run_date = $("#run_date option:selected");
+		var run_date = $("#run_date option:selected")[0];
 	
 	console.log('run_date selected: ' + run_date);
 	
